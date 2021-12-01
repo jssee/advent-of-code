71e9b114e166fb842345a01b26b0ae4cdea25a4e
Jesse Hoyos <jesse@mojotech.com>
2021-12-01 16:49:24 -0500

    day one

---
+++ 2021/1/init.lua
@@ -0,0 +1,22 @@
+local file = "./input.txt"
+local nums = {}
+
+for num in io.lines(file) do
+  table.insert(nums, tonumber(num))
+end
+
+function Count(input, batch)
+  local count = 0
+  for i=1, #input do
+    local x = batch + i
+    if input[x] ~= nil and input[x] > input[i] then
+      count = count + 1
+    end
+  end
+  return count
+end
+
+print(Count(nums, 1)) -- part 1
+print(Count(nums, 3)) -- part 2
+
+
